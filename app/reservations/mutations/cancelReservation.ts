397bf8de51037a952385833dd2aebe1ab0671324
Akashdeep Singh Kalra <44468736+adsk3003@users.noreply.github.com>
2022-04-30 03:27:45 +0530

    Reservation Service now using api calls (#7)
    Co-authored-by: Sharath Chandra <tnfssc@gmail.com>

--- app/reservations/mutations/cancelReservation.ts
+++ app/reservations/mutations/cancelReservation.ts
@@ -1,6 +1,9 @@
 import { Ctx } from "blitz"
 import db, { Reservation } from "db"
 
+// import { Reservation } from "db"
+import api from "../service"
+
 export type CancelReservationInput = Pick<Reservation, "id">
 
 const cancelReservation = async ({ id }: CancelReservationInput, ctx: Ctx) => {
@@ -19,5 +22,19 @@ const cancelReservation = async ({ id }: CancelReservationInput, ctx: Ctx) => {
 
   // @TODO Refund
 }
+
+// const cancelReservation = async ({ id }: CancelReservationInput, ctx: Ctx) => {
+//   const { user } = ctx
+//   if (!user) return
+//   if (user.role === "CUSTOMER") {
+//     if (await db.reservation.findFirst({ where: { id, customer_id: user.id } })) {
+//       return await api.get<Reservation>("/4", { id }).data
+//     }
+//   }
+//   if (user.role === "MANAGER")
+//     return await api.get<Reservation>("/4", { id }).data
+
+//   // @TODO Refund
+// }
 
 export default cancelReservation
--- app/reservations/mutations/deleteReservation.ts
+++ app/reservations/mutations/deleteReservation.ts
@@ -1,12 +1,20 @@
 import { Ctx } from "blitz"
-import db, { Reservation } from "db"
+// import db, { Reservation } from "db"
+import { Reservation } from "db"
+import api from "../service"
 
 export type DeleteReservationInput = Pick<Reservation, "id">
+
+// const deleteReservation = async ({ id }: DeleteReservationInput, ctx: Ctx) => {
+//   const { user } = ctx
+//   if (!user) return
+//   if (user.role === "MANAGER") return await db.reservation.delete({ where: { id } })
+// }
 
 const deleteReservation = async ({ id }: DeleteReservationInput, ctx: Ctx) => {
   const { user } = ctx
   if (!user) return
-  if (user.role === "MANAGER") return await db.reservation.delete({ where: { id } })
+  if (user.role === "MANAGER") return await api.delete<Reservation>("/", { params: { id } }).data
 }
 
 export default deleteReservation
