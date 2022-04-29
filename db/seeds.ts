ee412ca2616ac4bef5841c915b4594bf7c6aa82b
tnfssc <tnfssc@gmail.com>
2022-04-29 22:00:45 +0530

    Seeds

--- db/seeds.ts
+++ db/seeds.ts
@@ -3,13 +3,62 @@ import db from "."
 import faker from "@faker-js/faker"
 
 const seed = async () => {
-  for (let i = 0; i < 10; i++) {
-    await db.room.create({
-      data: {
-        number: `${faker.datatype.number({ max: 999, min: 100 })}`,
+  await db.room.createMany({
+    data: [
+      {
+        number: `${faker.datatype.number({ min: 100, max: 999 })}`,
+        suite: "Elite",
+        capacity: 2,
       },
-    })
-  }
+      {
+        number: `${faker.datatype.number({ min: 100, max: 999 })}`,
+        suite: "Luxury",
+        capacity: 2,
+      },
+      {
+        number: `${faker.datatype.number({ min: 100, max: 999 })}`,
+        suite: "Premium",
+        capacity: 3,
+      },
+      {
+        number: `${faker.datatype.number({ min: 100, max: 999 })}`,
+        suite: "VIP",
+        capacity: 2,
+      },
+    ],
+    skipDuplicates: true,
+  })
+  const users = new Array(0).map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    firstName: faker.name.findName(),
+    phone: faker.phone.phoneNumber(),
+  }))
+  await db.account.createMany({
+    data: users.map(({ id }) => ({
+      provider: "seed",
+      providerAccountId: "NaN",
+      type: "test",
+      userId: id,
+    })),
+    skipDuplicates: true,
+  })
+  await db.user.createMany({
+    data: users.map(({ email, id, name }) => ({
+      id,
+      name: name,
+      email: email,
+    })),
+    skipDuplicates: true,
+  })
+  await db.customer.createMany({
+    data: users.map(({ phone, id, name }) => ({
+      firstName: name,
+      id,
+      phone,
+    })),
+  })
 }
 
 export default seed
