7e6c745956c10145f5fa298db6855356a6bac397
rohitreddy-k <es18btech11010@iith.ac.in>
2022-04-30 00:55:06 +0530

    Room Service Added

--- app/rooms/queries/getAllRooms.ts
+++ app/rooms/queries/getAllRooms.ts
@@ -1,12 +1,19 @@
 import { Ctx } from "blitz"
-import db from "db"
+import db, { Room } from "db"
+import api from "../service"
 
 import getRooms from "./getRooms"
 
-const getAllRooms = async ({}, ctx: Ctx) => {
+/* const getAllRooms = async ({}, ctx: Ctx) => {
   const { user } = ctx
   if (!user) return
   return getRooms({}, ctx)
+} */
+
+const getAllRooms = async ({}, ctx: Ctx): Promise<Room[] | null | undefined> => {
+  const { user } = ctx
+  if (!user) return
+  if (user.role === "MANAGER") return (await api.get("/")).data
 }
 
 export default getAllRooms
